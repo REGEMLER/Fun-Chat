aa478e643a048389b45a142855f772e8682e0821
Yar Night <yaroslav239@gmail.com>
2024-04-16 20:16:20 +0300

    feat: add search and  sudden disconnection event handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import './index.css';
 import { launcher } from './app/app';
+import { createModal } from './app/view/modal/modal';
 
-export const socket = new WebSocket('ws://127.0.0.1:4000');
+export let socket = new WebSocket('ws://127.0.0.1:4000');
 
 socket.addEventListener('open', () => {
     launcher();
+});
+
+socket.addEventListener('close', () => {
+    createModal('A sudden disconnection from the server occurs!');
+    socket = new WebSocket('ws://127.0.0.1:4000');
 });
